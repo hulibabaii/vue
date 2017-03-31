6e8193b4871b9364c3d3511300be51f8b69048fa
hulibabaii <hulibabaii@gmail.com>
2017-03-31 11:26:50 +0800

    2222

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -6,6 +6,8 @@ public class Test {
 		// TODO Auto-generated method stub
 
 		System.out.print("Hellow World!");
+		System.out.print("Hellow World!");
+		System.out.print("Hellow World!");
 		
 //		1321321
 	}
